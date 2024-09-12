3c0a1ea6909d2c0b8178c3fbb199b3bcd00161ef
SuhasH123456 <suhas.h@vectorstack.co>
2024-09-12 20:09:06 +0530

    tested

--- varanasi_website/src/App.js
+++ varanasi_website/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import imagesVaranasi from '../src/varanasi_image.jpg';
 
 function App() {
   return (
     <>
       <h1 align="center">Varanasi</h1>
       <h2 align="center">Welcome to the store</h2>
+      <div align="center">
+        {/* Add the image here */}
+        <img src={imagesVaranasi} alt="Store" width="300px" height="300px" />
+      </div>
     </>
   );
 }
