ea45719e347d28f178aef514140b4f5f8e9aa810
Suhas H <suhasharish9090@gmail.com>
2024-09-12 20:31:59 +0530

    Update App.js

--- varanasi_website/src/App.js
+++ varanasi_website/src/App.js
@@ -8,7 +8,6 @@ function App() {
       <h1 align="center">Varanasi</h1>
       <h2 align="center">Welcome to the store</h2>
       <div align="center">
-        {/* Add the image here */}
         <img src={imagesVaranasi} alt="Store" width="300px" height="300px" />
         <img src={imagesVaranasi1} alt="Store" width="300px" height="300px" />
       </div>
