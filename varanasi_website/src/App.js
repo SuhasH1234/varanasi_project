838a1fddd49b22f770f6a7e0683c5cdefb0e0f2a
SuhasH123456 <suhas.h@vectorstack.co>
2024-09-12 19:42:17 +0530

    tested

---
+++ varanasi_website/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 align="center">Varanasi</h1>
+      <h2 align="center">Welcome to the store</h2>
+    </>
+  );
+}
+
+export default App;
